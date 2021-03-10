0179f1badb56195c2335e42bf2066f50ff64f2a7
nozimbek <75173590+nozimbek-sultonov@users.noreply.github.com>
2021-03-10 14:11:28 +0500

    none
    none

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// alert("Saytimga xush kelib siz!")
+
+
+// let savol = prompt("Savol ? (nechi yosh siz )") - 0
+// if(savol >= 18){
+//     alert("KIRING");
+// }
+// else if(savol < 18){
+//     alert("Siz hali voyaga yetmagan siz");
+// }
